6fb2d5cc7f988cfec4318956259fbb831952521d
Evgeny1337 <genya-sozykin@mal.ru>
2025-07-08 23:09:00 +0500

    preview added

--- places/admin.py
+++ places/admin.py
@@ -1,17 +1,31 @@
 from django.contrib import admin
 from .models import Place, PlaceImage
-
+from django.utils.safestring import mark_safe
 
 class PlaceImageInline(admin.TabularInline):
     model = PlaceImage
+    field = ('image','preview')
+    readonly_fields = ('preview',)
+    extra = 1
+    def preview(self, obj):
+        if obj.image and hasattr(obj.image,'url'):
+            return mark_safe(f'<img src="{obj.image.url}" height="200" />')
+        return "Изображение отсутсвует"
+    preview.short_description = "Превью"
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
     inlines = [
         PlaceImageInline,
     ]
-
+    
 @admin.register(PlaceImage)
 class PlaceImageAdmin(admin.ModelAdmin):
-    pass
+    readonly_fields = ("preview",)
+
+    def preview(self, obj):
+        if obj.image and hasattr(obj.image,'url'):
+            return mark_safe(f'<img src="{obj.image.url}" height="200" />')
+        return "Изображение отсутсвует"
+    preview.short_description = "Превью"
 
