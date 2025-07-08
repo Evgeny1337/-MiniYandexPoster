2dc4163dc0e8452ba6845db9b5f127f93b40b497
Evgeny1337 <genya-sozykin@mal.ru>
2025-07-08 12:53:21 +0500

    Added getting places from the database

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,35 +1,23 @@
 from django.shortcuts import render
 from places.models import Place
 from django.templatetags.static import static
-
+import json
 def show_start(request):
+    places = Place.objects.all()
+    features = [{
+                "type":"Feature", 
+                 "geometry": { 
+                     "type": "Point",
+                     "coordinates": [place.latitude, place.longitude]
+                     }, 
+                  "properties": {
+                      "title": place.title,
+                      "placeId":f"{place.title}",
+                      "detailsUrl": static('places/moscow_legends.json')
+                      }
+                } for place in places]
     geojson = {
         "type": "FeatureCollection",
-        "features": [
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.62, 55.793676]
-                },
-                "properties": {
-                    "title": "«Легенды Москвы",
-                    "placeId": "moscow_legends",
-                    "detailsUrl": static('places/moscow_legends.json')
-                }
-            },
-            {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [37.64, 55.753676]
-                },
-                "properties": {
-                    "title": "Крыши24.рф",
-                    "placeId": "roofs24",
-                    "detailsUrl": static('places/roofs24.json')
-                }
-            }
-        ]
+        "features": features
     }
     return render(request, 'index.html', {'geojson': geojson})
