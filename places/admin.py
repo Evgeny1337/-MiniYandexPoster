ef4f1080a061e730ddaea2999b44b9857cc378ce
Evgeny1337 <genya-sozykin@mal.ru>
2025-07-14 21:18:57 +0500

    replaced with format_html

--- places/admin.py
+++ places/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from .models import Place, PlaceImage
 from django.utils.safestring import mark_safe
-from adminsortable2.admin import SortableTabularInline, SortableAdminMixin, SortableAdminBase
+from django.utils.html import format_html
+from adminsortable2.admin import SortableTabularInline, SortableAdminBase
 from tinymce.widgets import TinyMCE
+
 
 class PlaceImageInline(SortableTabularInline):
     model = PlaceImage
@@ -12,7 +14,8 @@ class PlaceImageInline(SortableTabularInline):
 
     def preview(self, obj):
         if obj.image and hasattr(obj.image,'url'):
-            return mark_safe(f'<img src="{obj.image.url}" height="200" />')
+            img = format_html(f'<img src="{obj.image.url}" height="200" />')
+            return img
         return "Изображение отсутсвует"
     preview.short_description = "Превью"
 
@@ -33,7 +36,8 @@ class PlaceImageAdmin(admin.ModelAdmin):
     readonly_fields = ("preview",)
     def preview(self, obj):
         if obj.image and hasattr(obj.image,'url'):
-            return mark_safe(f'<img src="{obj.image.url}" height="200" />')
+            img = format_html(f'<img src="{obj.image.url}" height="200" />')
+            return img
         return "Изображение отсутсвует"
     preview.short_description = "Превью"
 
