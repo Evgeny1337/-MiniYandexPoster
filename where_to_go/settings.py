e7e995864f4acfafcd7d51ba122ea2e2ac3f2b46
Evgeny1337 <genya-sozykin@mal.ru>
2025-07-14 14:28:32 +0500

    moved views

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -1,108 +1,3 @@
-# """
-# Django settings for where_to_go project.
-# """
-
-# from pathlib import Path
-# import os
-# from environs import Env
-
-# env = Env()
-# env.read_env()
-
-# # Build paths
-# BASE_DIR = Path(__file__).resolve().parent.parent
-
-# # Security
-# SECRET_KEY = env.str('SECRET_KEY', default='django-insecure-fallback-key')  
-# DEBUG = env.bool('DEBUG', default=False)
-# ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
-
-# # Application definition
-# INSTALLED_APPS = [
-#     'django.contrib.admin',
-#     'django.contrib.auth',
-#     'django.contrib.contenttypes',
-#     'django.contrib.sessions',
-#     'django.contrib.messages',
-#     'django.contrib.staticfiles',  
-#     'adminsortable2',
-#     'tinymce',
-#     'places'
-# ]
-
-
-# MIDDLEWARE = [
-#     'django.middleware.security.SecurityMiddleware',
-#     'whitenoise.middleware.WhiteNoiseMiddleware',  
-#     'django.contrib.sessions.middleware.SessionMiddleware',
-#     'django.middleware.common.CommonMiddleware',
-#     'django.middleware.csrf.CsrfViewMiddleware',
-#     'django.contrib.auth.middleware.AuthenticationMiddleware',
-#     'django.contrib.messages.middleware.MessageMiddleware',
-#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-# ]
-
-# ROOT_URLCONF = 'where_to_go.urls'
-
-# TEMPLATES = [
-#     {
-#         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-#         'DIRS': [os.path.join(BASE_DIR, 'templates')],
-#         'APP_DIRS': True,
-#         'OPTIONS': {
-#             'context_processors': [
-#                 'django.template.context_processors.debug',
-#                 'django.template.context_processors.request',
-#                 'django.contrib.auth.context_processors.auth',
-#                 'django.contrib.messages.context_processors.messages',
-#             ],
-#         },
-#     },
-# ]
-
-# WSGI_APPLICATION = 'where_to_go.wsgi.application'
-
-# # Database
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-# # Password validation
-# AUTH_PASSWORD_VALIDATORS = [
-#     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-#     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-#     {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-#     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
-# ]
-
-# # Internationalization
-# LANGUAGE_CODE = 'ru-ru'  
-# TIME_ZONE = 'Europe/Moscow'  
-# USE_I18N = True
-# USE_TZ = True
-
-# # Static files 
-# STATIC_URL = '/static/'  
-# STATIC_ROOT = env.str('STATIC_ROOT', default=str(BASE_DIR / 'staticfiles'))  
-
-# MEDIA_URL = '/media/'  
-# MEDIA_ROOT = env.str('MEDIA_ROOT', default=str(BASE_DIR / 'media'))  
-
-
-# if DEBUG:
-#     STATICFILES_DIRS = [
-#         BASE_DIR / "static",
-#         BASE_DIR / "static/places",
-#     ]
-# else:
-#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-
-# DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 """
 Django settings for where_to_go project.
 
@@ -115,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+from pathlib import Path
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 from environs import Env
